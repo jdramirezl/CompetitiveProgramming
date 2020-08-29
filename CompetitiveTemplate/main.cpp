0eb78049599aa982ee8199cc70619e29b12c6697
Julian David Ramirez Lopera <jdramirezl@eafit.edu.co>
2020-08-29 15:40:25 -0500

    binary

--- CompetitiveTemplate/main.cpp
+++ CompetitiveTemplate/main.cpp
@@ -2,7 +2,7 @@
 
 #define D(x) cout << #x << " = " << x << endl;
 #define ios ios_base::sync_with_stdio(0), cin.tie(0);
-#define forn(i, n) for (int i = 0; i < (int)n; i++)
+#define forn(i, n) for (int i = 0; i < (int)n; ++i)
 #define all(v) v.begin(), v.end()
 #define formap(map) for (const auto &[key, value] : map)
 #define ms(ar, val) memset(ar, val, sizeof ar)
@@ -11,11 +11,10 @@ typedef long double ld;
 
 using namespace std;
 
-int main()
+int binary(vector<string> contacts, string name, int size)
 {
-    cout << "Hello world";
 }
 
-void problem()
+int main()
 {
 }
