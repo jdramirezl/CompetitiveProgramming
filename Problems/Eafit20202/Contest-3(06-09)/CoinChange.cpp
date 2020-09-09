6e0d52dc64e27acfd944877a1d2adb5b86e8c42b
Julian David Ramirez Lopera <jdramirezl@eafit.edu.co>
2020-09-09 14:05:18 -0500

    Added coin change problems

--- Problems/Eafit20202/Contest-3(06-09)/CoinChange.cpp
+++ Problems/Eafit20202/Contest-3(06-09)/CoinChange.cpp
@@ -14,30 +14,42 @@ typedef long double ld;
 using namespace std;
 
 int dp(int n){
+    int pos = 0;
     int coins[] = {1, 5, 10, 25, 50};
-    int dp[4][n];
-    dp[0][0] = 0; dp[1][0] = 0; dp[2][0] = 0; dp[3][0] = 0; dp[4][0] = 0; 
+    int dp[5][n];
 
-    forn(1,n){
-        for(int j = 0){
-
-        }
+    fill(*dp, *dp + 5*n, 0);
+    for(int i = 0; i<n; ++i){
+        dp[0][i] = 1;
     }
 
-    forn(0, 5){
-        if(n-coins[i]>=0){
-            forn(0,n){
+    for(int i = 1; i<5; ++i){
+        if(coins[i] <= n){
+            pos++;
+            dp[i][coins[i]-1] = 1 + dp[i-1][coins[i]-1];
 
+            for(int y = 0; y<coins[i]-1; ++y){
+                dp[i][y] += dp[i-1][y];
+            }
+
+            for(int j = coins[i]; j<n; ++j){
+                dp[i][j] += (dp[i-1][j]+dp[i][j-coins[i]]);
             }
         }
     }
+
+    return dp[pos][n-1];
+
 }
 
 
 int main()
 {
     int n;
-    while (cin >> n){
-
+    string num;
+    while (getline(cin, num) && !num.empty()){
+        n = stoi(num);
+        cout << dp(n) << "\n";
     }
+    
 }
