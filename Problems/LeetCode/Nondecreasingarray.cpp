f87179c77b9bbd5756508c81a7972a80e5ec396b
Julian <jdramirezl@eafit.edu.co>
2020-11-02 23:33:14 -0500

    New problems leetcode

--- Problems/LeetCode/Nondecreasingarray.cpp
+++ Problems/LeetCode/Nondecreasingarray.cpp
@@ -7,52 +7,45 @@
 using namespace std;
 
 class Solution {
-    public:
-        bool checkPossibility(vector<int>& nums) {
-            int n = 0, size = nums.size(), high = nums[0], me, ma, mev, mav;
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int n = 0, size = nums.size(), high = nums[0], me =-1, ma, mev, mav;
 
-            if(size <= 2) return true;
+        if(size <= 2) return true;
 
-            //Find breaking point
-            for(int i = 0; i<nums.size()-1; ++i){
-                if(nums[i] > nums[i+1]) {
-                    me = i;
-                    mev = nums[i];
-                    ma = i+1;
-                    mav = nums[i+1];
-                    break;
-                }
+        //Find breaking point
+        for(int i = 0; i<nums.size()-1; ++i){
+            if(nums[i] > nums[i+1]) {
+                me = i;
+                mev = nums[i];
+                ma = i+1;
+                mav = nums[i+1];
+                break;
             }
-
-            bool nf = false, ns = false;
-
-            cout << ma << " " << me << endl;
-
-            //Removing first
-            cout << "First"<< endl;
+        }
+        
+        bool nf = false, ns = false;
+        
+        if(me != -1){
             nums[ma] = mev;
             for(int i = 0; i<nums.size()-1; ++i){
-                cout << nums[i] << " " << nums[i+1]<< endl;
                 if(nums[i] > nums[i+1]) {
                     nf = true;
                     break;
                 }
             }
-
-            //removing second
-            cout << "Second"<< endl;
+            
             nums[me] = mav;
             nums[ma] = mav;
             for(int i = 0; i<nums.size()-1; ++i){
-                cout << nums[i] << " " << nums[i+1]<< endl;
                 if(nums[i] > nums[i+1]) {
                     ns = true;
                     break;
                 }
             }
-
-            cout << ns << " " << nf;
-
-            return !(nf && ns);
+            
         }
+        
+        return !(nf && ns);
+    }
 };
