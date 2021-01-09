a4390e1742c1e11efe568c3337249db7d648531f
Julian David Ramirez Lopera <jdramirezl@eafit.edu.co>
2021-01-09 11:55:19 -0500

    New Problems

---
+++ Problems/Eafit2020-2/Eliminatoria(8/UNFinals.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define D(x) cout << #x << " = " << x << endl;
+#define ios ios_base::sync_with_stdio(0), cin.tie(0);
+#define forn(x, n) for (int i = x; i < (int)n; ++i)
+#define all(v) v.begin(), v.end()
+#define allgreater(v) v.begin(), v.end(), greater<int>()
+#define formap(map) for (const auto &[key, value] : map)
+#define ms(ar, val) memset(ar, val, sizeof ar)
+#define vvi vector<vector<int>>
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+int main()
+{
+}
